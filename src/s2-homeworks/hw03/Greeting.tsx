41a5650939c63c236e580c46c78887a1935522f3
AlexandrIvanov <alexandr.1996@list.ru>
2022-10-27 17:57:14 +0300

    homework 3

---
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -0,0 +1,79 @@
+import React, {ChangeEvent, KeyboardEvent} from 'react'
+import s from './Greeting.module.css'
+
+type GreetingPropsType = {
+    name: string
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void
+    addUser: () => void
+    onBlur: () => void
+    onEnter: (e: KeyboardEvent<HTMLInputElement>) => void
+    error: string | null
+    totalUsers: number
+    lastUserName?: string
+}
+
+// презентационная компонента (для верстальщика)
+const Greeting: React.FC<GreetingPropsType> = (
+    {
+        name,
+        setNameCallback,
+        addUser,
+        onEnter,
+        onBlur,
+        error,
+        totalUsers,
+        lastUserName,
+    } // деструктуризация пропсов
+) => {
+    const inputClass = error ? s.errorInput : ''
+
+    const onEnterIn = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.code === 'Enter') {
+            addUser()
+        }
+    }
+
+    return (
+        <div id={'hw3-form'} className={s.greetingForm}>
+            <div className={s.text}>
+                {'Людей добавили: '}
+                <span id={'hw3-users-total'}>
+                    {totalUsers}
+                </span>
+            </div>
+
+            <div className={s.inputAndButtonContainer}>
+                <div>
+                    <input
+                        id={'hw3-input'}
+                        value={name}
+                        onChange={setNameCallback}
+                        className={error ? inputClass : s.input}
+                        onKeyDown={onEnter}
+                        onBlur={onBlur}
+                    />
+                    <div id={'hw3-error'} className={s.error}>
+                        {error}
+                    </div>
+                </div>
+
+                <button
+                    id={'hw3-button'}
+                    onClick={addUser}
+                    className={s.button}
+                    disabled={!name.trim()}
+                >
+                    Add
+                </button>
+            </div>
+
+            {lastUserName && (
+                <div className={s.greeting}>
+                    Привет <span id={'hw3-last-user'}>{lastUserName}</span>!
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Greeting
